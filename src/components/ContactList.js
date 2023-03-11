c21726aa88a1cf30e3bbc261c5e3aa086186d43d
irfanramadhan1999 <irfanramadhani@students.amikom.ac.id>
2023-03-11 09:04:37 +0700

    menambah fitur hapus kontak

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,16 +2,19 @@ import React from "react";
 import ContactItem from "./ContactItem";
 
 
-function ContactList({contacts}) {
-    return (
-        <div className="contact-list">
-            {
-                contacts.map((contact) => (
-                    <ContactItem key={contact.id} {...contact} />
-                ))
-            }
-        </div>
-    );
+function ContactList({ contacts, onDelete }) {
+  return (
+    <div className="contact-list">
+      {contacts.map((contact) => (
+        <ContactItem
+          key={contact.id}
+          {...contact}
+          id={contact.id}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ContactList;
---
+++ src/components/CounterApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+class CounterApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>TODO: selesaikan component-nya!</p>
+      </div>
+    );
+  }
+}
+
+createRoot(document.getElementById("root"));
+
+export default CounterApp;
